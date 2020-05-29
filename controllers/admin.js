91c5f116d08be02c54dae7e28a64639ef7fd83af
Jeff Jackson <jj@rtdronline.com>
2020-05-29 18:20:55 -0500

    adds edit customer route and basic ejs page

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,44 +7,49 @@ router.get('/', (req, res) => {
     res.render('admin/index');
 });
 
-//root customer administration page
+//ROOT customer administration page
 router.get('/cust', (req, res) => {
     res.render('admin/cust/index');
 });
 
-//root product administration page
+//ROOT product administration page
 router.get('/product', (req, res) => {
     res.render('admin/product/index');
 });
 
-//new customer page
+//NEW customer page
 router.get('/cust/new', (req, res) => {
     res.render('admin/cust/new');
 });
 
-//new product page
+//NEW product page
 router.get('/product/new', (req, res) => {
     res.render('admin/product/new');
 });
 
-//create customer route
+//CREATE customer route
 router.post('/cust', (req, res) => {
     res.redirect('admin/cust');
 });
 
-//create product route
+//CREATE product route
 router.post('/product', (req, res) => {
     res.redirect('admin/product');
 });
 
-//create customer show page
+//SHOW customer show page
 router.get('/cust/:id', (req, res) => {
     res.render('admin/cust/show');
 });
 
-//create product show page
+//SHOW product show page
 router.get('/product/:id', (req, res) => {
     res.render('admin/product/show');
+});
+
+//EDIT customer show page
+router.get('/cust/:id/edit', (req, res) => {
+    res.render('admin/cust/edit');
 });
 
 module.exports = router;
