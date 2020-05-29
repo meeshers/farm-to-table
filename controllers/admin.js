402cc5450c8591b6f7de24876374f7a79ee36914
Jeff Jackson <jj@rtdronline.com>
2020-05-29 18:03:47 -0500

    added root routes to main administration, customer administration, product administration

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,5 +2,20 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 
+//root administration page
+router.get('/', (req, res) => {
+    res.render('admin/index');
+});
+
+//root customer administration page
+router.get('/cust', (req, res) => {
+    res.render('admin/cust/index');
+});
+
+//root product administration page
+router.get('/product', (req, res) => {
+    res.render('admin/product/index');
+});
+
 
 module.exports = router;
