6af403b9ce16dcc4987fe2d1abf588b61c910a24
Jeff Jackson <jj@rtdronline.com>
2020-06-01 14:23:00 -0500

    adds styling to admin products

---
+++ public/scripts/admin.js
@@ -0,0 +1,13 @@
+$('.delete-prod').click((event) => {
+    // event.preventDefault();
+    // const $el = $(this);
+    // const x = $el.attr("id");
+    // const y = event.pageY;
+    // //let offset = event.offsetTop;
+    // console.log(x);
+    // //alert(x);
+    // alert("Are you sure you want to delete this item?\nThe position is: " + x + " : " + y);
+
+    //$('.delete-item').css("top", `${y}px`);
+    //$('.delete-item').css("left", `${x}px`);
+});
