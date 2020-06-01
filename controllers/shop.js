53abf35bd8cff9db9cddb9ffd1e9202b9aed495c
Michelle <m.tran1223@gmail.com>
2020-06-01 12:13:13 -0700

    fixed more styling for show page

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,58 +1,74 @@
 const express = require('express');
 const router = express.Router();
+const functions = require('../middleware/external');
 const db = require('../models');
 
 // Home page route
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
   res.render('shop/index');
 })
 
 // View all products route
-router.get('/products', async (req,res)=>{
-  try{
+router.get('/products', async (req, res) => {
+  try {
     const allProducts = await db.Products.find({});
-    res.render('shop/product', {product: allProducts});
-  } catch(error) {
+    res.render('shop/product', { product: allProducts });
+  } catch (error) {
     console.log(error);
-    res.send({message: "Internal Server Error!"});
+    res.send({ message: "Internal Server Error!" });
   }
 
 })
 
 // product show route
-router.get('/product/:id', async (req,res)=>{
+router.get('/product/:id', async (req, res) => {
   try {
     const foundProduct = await db.Products.findById(req.params.id);
-    res.render('shop/show', {product: foundProduct});
-  } catch(error){
+    let birthDate = (foundProduct.birthDate !== null)? functions.getDate(foundProduct.birthDate, true) : '';
+    let readyDate = (foundProduct.readyDate !== null)? functions.getDate(foundProduct.readyDate, true) : '';
+    
+    const product = {
+        _id: foundProduct._id,
+        name: foundProduct.name,
+        description: foundProduct.description,
+        price: foundProduct.price,
+        birthDate: birthDate,
+        readyDate: readyDate,
+        available: foundProduct.available,
+        growthNotes: foundProduct.growthNotes,
+        img: foundProduct.img
+    }
+
+    res.render('shop/show', {product: product});
+  } catch (error) {
     console.log(error);
-    res.send({message: "Internal Server Error!"});
+    res.send({ message: "Internal Server Error!" });
   }
 
 })
 
 // about page route
-router.get('/about', (req,res)=>{
+router.get('/about', (req, res) => {
   res.render('shop/about');
 })
 
 // subscribe route
-router.get('/subscribe', (req,res)=>{
+router.get('/subscribe', (req, res) => {
   res.render('shop/subscribe');
 })
 
 // log in route
-router.get('/login', (req,res)=>{
+router.get('/login', (req, res) => {
   res.render('shop/login');
 })
 
 // view cart route
-router.get('/cart', (req,res)=>{
+router.get('/cart', (req, res) => {
   res.render('shop/cart');
 })
 
 // user page route
-router.get('/user', (req,res)=>{
+router.get('/user', (req, res) => {
   res.render('shop/user');
 })
 
