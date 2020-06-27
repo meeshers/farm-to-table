dd5f822bfcfd082c28ac5be682b1e8e789b2c052
Jeff Jackson <jj@rtdronline.com>
2020-06-27 12:46:48 -0500

    changes some styling

--- middleware/adminAuthReq.js
+++ middleware/adminAuthReq.js
@@ -1,5 +1,5 @@
 module.exports = function(req,res,next) {
-    //if(!req.session.currentUser && req.session.currentUser.admin)
+    // if(!req.session.currentUser && req.session.currentUser.admin)
     if(!req.session.adminUser)
     {
         return res.redirect("/admin/login");
